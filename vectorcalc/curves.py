f8402acbdea00e5e8860415fcb4f46b6e5797115
xan3c <kenny@kennymoc.xyz>
2023-03-01 12:15:14 -0700

    more abstract base classes

--- vectorcalc/curves.py
+++ vectorcalc/curves.py
@@ -20,17 +20,44 @@ class Curve():
         #Defining variables in class-scope
         self.begin_point = domain_interval[0]
         self.end_point = domain_interval[1]
-        self.curve_dim = len(curve_func)     #The dimension of the vector space of the curve
+        self.curve_func = curve_func
+        self.curve_dim = len(self.curve_func)     #The dimension of the vector space of the curve
 
         return None
+
+    def dimension(self):
+        return self.curve_dim
+
+    def _evaluate(self, point, curve_func):
+        """Non-public utility function to find the value of the curve_func at a point on the curve"""
+        pass
     
     def __add__(self, second_curve: Curve):
         """Overloads the addition operator to add curves together"""
         pass
 
+    def curve_value(self, point, curve_func):
+        """Public function to find the value of the curve at a point"""
+
+        #Which curve_func is used on the interval?
+        #Within the domain?
+        pass
+
+    def __getitem__(self, key):
+        #return self.curve_func[key] ?
+        pass
+
 def scalar_integrate(curve : Curve, func, num : int) -> int:
     pass
 
+def vector_integrate(curve: Curve, func, num: int) -> int:
+    pass
+
+def curve_length(curve: curve, num:int) -> int:
+    pass
+
+def curve_derivative(curve: curve, num:int) -> list: #is it a list?
+    pass
 
 
 
