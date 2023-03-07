8be18793da92fa2ad4d1b1358d29525c79981f83
xan3c <kenny@kennymoc.xyz>
2023-03-07 16:23:26 -0700

    more changes

--- tests/test_curve.py
+++ tests/test_curve.py
@@ -3,15 +3,23 @@ import vectorcalc as vc
 
 class TestCurveValues(unittest.TestCase):
     def runTest(self):
-      curve = vc.Curve([0, 1], [lambda x: x, lambda x: x])
+      curve = vc.Curve([0, 1], [lambda x: 1, lambda x: x])
 
       #Tests that the domain is set correctly
       self.assertEqual(curve.begin_point, 0, "incorrect beginning point")
       self.assertEqual(curve.end_point, 1, "incorrect end point")
 
       #Tests that the curve evaluates the paramaterization correctly
+      self.assertEqual(curve.dim, 2, 'incorrect dimension')
+      self.assertEqual(curve.value(1), [1,1], 'incorrect evaluation')
 
+      #Tests curve addition
+      second_curve = vc.Curve([1, 3], [lambda x: 1, lambda x: 2*x - 1])
+      curve += second_curve
 
+      self.assertEqual(curve.end_point, 3, 'incorrect endpoint')
+      self.assertEqual(curve.value(1), [1, 1], 'incorrect boundary point')
+      self.assertEqual(curve.value(3), [1, 5], 'incorrect curve extension')
 
 
 unittest.main()
--- vectorcalc/curves.py
+++ vectorcalc/curves.py
@@ -8,60 +8,65 @@ class Curve():
     curve_dim : 
     """
     def __init__(self, domain_interval:list, curve_func:list) -> None:
-        """Inits Curve with a domain interval and a parametrization of the curve"""
+        """Init a curve with a domain interval and a parametrization of the curve for this domain"""
         
-        #Some error handling
+        # Some error handling to ensure the proper inputs
         if not len(domain_interval) == 2:
             raise Exception('Domain interval can only consist of two points')
+        if not domain_interval[1] >= domain_interval[0]:
+            raise Exception('End point of domain must be larger than begin point')
         for param in curve_func:
-            if not callable(param) or type(param) != int or type(param) != float:
-                raise Exception('Parametrization of curve must be callable or a constant number')
+            if not callable(param):
+                raise Exception('Parametrization of curve must be callable')
 
-        #Defining variables in class-scope
-        self.begin_point = domain_interval[0]
-        self.end_point = domain_interval[1]
-        self.curve_dim = len(curve_func)     #The dimension of the vector space of the curve
+        #Defining instance variables
+
+        # A dictionary to store where all the breaks in the domain are at.
+        # This will be useful when we add curves together, so we know when our curve_func parametrization changes
+        self.breaks = {}                 
+        self.breaks[domain_interval[0]] = curve_func         #The start of the domain maps to curve_func
+        self.breaks[domain_interval[1]] = curve_func     
+
+        # The domain of the curve
+        self.begin_point = list(self.breaks)[0]
+        self.end_point =  list(self.breaks)[-1]
+
+
+        self.dim = len(curve_func)     # The dimension of the vector space of the curve
 
         return None
 
-    def _curve_func():
-        pass
+    def _curve_func(self, point:float):
+        domain = min(i for i in list(self.breaks) if i >= point) # Finds the first point of the domain interval in which the point lies
 
-    def dimension(self) -> int:
-        """Returns the dimension of the curve."""
-        return self.curve_dim
+        return self.breaks[domain]          # Returns the curve_func value at that point.
 
     def _evaluate(self, point:float, curve_func) -> list:
-        """Non-public utility function to find the value of the curve_func at a point"""
+        """Non-public utility function to find the value of the curve at a point"""
         value = [func(point) for func in curve_func]
         return value
-    
-    def __add__(self, second_curve):
-        """Overloads the addition operator to add curves together"""
-        pass
 
-    def curve_value(self, point:float) -> list:
+    def value(self, point:float) -> list:
         """Public function to find the value of the curve at a point"""
 
         if point < self.begin_point or point > self.end_point:
             raise Exception('Point is outside domain of curve')
 
-        #TODO implement test for two curves
+        return self._evaluate(point, self._curve_func(point))
 
-
-
+    def __add__(self, second_curve):
+        """Overloads the addition operator to add curves together"""
         
+        #Some error handling
+        if not second_curve.begin_point == self.end_point:
+            raise Exception('Domain of second curve does not begin at the end of the first curve.')
+        if not second_curve.value(second_curve.begin_point) == self.value(self.end_point):
+            raise Exception('Curves must join at endpoints')
 
+        self.breaks.update(second_curve.breaks)
+        self.end_point = second_curve.end_point
 
-        
-
-        #Which curve_func is used on the interval?
-        #Within the domain?
-        pass
-
-    def __getitem__(self, key):
-        #return self.curve_func[key] ?
-        pass
+        return self      
 
 def scalar_integrate(curve : Curve, func, num : int) -> int:
     pass
